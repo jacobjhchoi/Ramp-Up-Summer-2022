2f471cfe7f338d38108731b45494ab3d59783496
lujiec2020 <lujiec2020@gmail.com>
2022-02-28 12:47:16 -0500

    hw3-again

---
+++ hw/hw03/tests/q1_6.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_6',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': ">>> pter_over_time.take(0).column(0).item(0) == 1994 \nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
